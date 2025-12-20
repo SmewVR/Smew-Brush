7a7447381865b436c2b41307511d96d90f3cd10d
unknown <heyitssmew@gmail.com>
2025-12-19 23:17:00 -0600

    added uvs mesh.uvs and vt, vn int vars

--- Export_Drawing.cs
+++ Export_Drawing.cs
@@ -6,6 +6,8 @@ using VRC.SDKBase;
 using VRC.Udon;
 using VRC.Udon.Common.Interfaces;
 
+using TMPro;
+
 namespace QvPen.UdonScript
 {
     public class Export_Drawing : UdonSharpBehaviour
@@ -25,13 +27,25 @@ namespace QvPen.UdonScript
         private Vector3[] mesh_normals;
 
         private int[] mesh_tris;
+
+        private Vector2[] mesh_uvs;
         private int v1;
         private int v2;
         private int v3;
+
+        private int vt1;
+        private int vt2;
+        private int vt3;
+
+        private int vn1;
+        private int vn2;
+        private int vn3;
+
         private int object_counter;
 
         private int vertexOffset;
-        public InputField export_text_field;
+        public TMP_InputField export_text_field;
+        // public TMP_Text export_text_display;
 
         public string Interact()
         {
@@ -44,7 +58,7 @@ namespace QvPen.UdonScript
         public string _Export()
         {
             formatPos = "";
-            formatPos += string.Format("# OBJ file created in the Smew Brush! VRChat world \v {0} \v https://vrchat.com/home/world/wrld_0097c2e4-634a-445b-a43d-9585cb6df959 \v\v", System.DateTime.Now.ToString());
+            formatPos += string.Format("# OBJ file created in the Smew Brush! VRChat world \n {0} \n https://vrchat.com/home/world/wrld_0097c2e4-634a-445b-a43d-9585cb6df959 \n\n", System.DateTime.Now.ToString());
             object_counter = 0;
             vertexOffset = 0;
 
@@ -64,11 +78,12 @@ namespace QvPen.UdonScript
                 mesh_vertices = testmesh.vertices;
                 mesh_tris = testmesh.triangles;
                 mesh_normals = testmesh.normals;
-
+                mesh_uvs = testmesh.uv;
+                
                 Debug.Log(mesh_vertices);
                 Debug.Log(mesh_vertices.Length);
 
-                formatPos += "o Object_" + object_counter.ToString() + "\v";
+                formatPos += "o Object_" + object_counter.ToString() + "\n";
                 object_counter++;
 
                 // adjust to keep the uv map
@@ -80,55 +95,58 @@ namespace QvPen.UdonScript
 
                 // export vertex data
                 for (int i = 0; i < mesh_vertices.Length; i++)
-                    formatPos += string.Format("v {0} {1} {2}", mesh_vertices[i].x, mesh_vertices[i].y, mesh_vertices[i].z) + "\v";
+                    formatPos += string.Format("v {0} {1} {2}", mesh_vertices[i].x, mesh_vertices[i].y, mesh_vertices[i].z) + "\n";
 
                 Debug.Log(formatPos);
 
                 // // export face data
                 // for (int i = 0; i < mesh_tris.Length; i += 3)
                 //     // Note: OBJ format uses 1-based 
-                //     formatPos += string.Format("f {0}/1/1 {1}/1/1 {2}/1/1", mesh_tris[i] + 1 + vertexOffset, mesh_tris[i + 1] + 1 + vertexOffset, mesh_tris[i + 2] + 1 + vertexOffset) + "\v";
+                //     formatPos += string.Format("f {0}/1/1 {1}/1/1 {2}/1/1", mesh_tris[i] + 1 + vertexOffset, mesh_tris[i + 1] + 1 + vertexOffset, mesh_tris[i + 2] + 1 + vertexOffset) + "\n";
                 
-                // added 4/22/2025
-                // Write UVs (simple planar mapping using x/z)
-                for (int i = 0; i < mesh_vertices.Length; i++)
-                {
-                    Vector3 v = mesh_vertices[i];
-                    float u = v.x;
-                    float vCoord = v.z;
-                    formatPos += $"vt {u} {vCoord}\v";
-                }
-
+                //NORMALS vn x y z
                 // Write normals (fallback if empty)
                 if (mesh_normals == null || mesh_normals.Length == 0)
                 {
                     for (int i = 0; i < mesh_vertices.Length; i++)
-                        formatPos += "vn 0 1 0\v"; // default up normal
+                        formatPos += "vn 0 1 0\n"; // default up normal
                 }
                 else
                 {
                     for (int i = 0; i < mesh_normals.Length; i++)
                     {
                         Vector3 n = mesh_normals[i];
-                        formatPos += $"vn {n.x} {n.y} {n.z}\v";
+                        formatPos += $"vn {n.x} {n.y} {n.z}\n";
                     }
                 }
+                // UVs vt x y
+                    // added 4/22/2025
+                // Write UVs (simple planar mapping using x/z)
+                for (int i = 0; i < mesh_uvs.Length; i++)
+                {
+                    Vector2 uv = mesh_uvs[i];
+                    float u = uv.x;
+                    float vCoord = uv.y;
+                    formatPos += $"vt {u} {vCoord}\n";
+                }
+
+                // FACES f / / /
                 // Write faces with 1-based indices and matching v/vt/vn
                 for (int i = 0; i < mesh_tris.Length; i += 3)
                 {
                     int v1 = mesh_tris[i] + 1 + vertexOffset;
                     int v2 = mesh_tris[i + 1] + 1 + vertexOffset;
                     int v3 = mesh_tris[i + 2] + 1 + vertexOffset;
-
-                    formatPos += $"f {v1}/{v1}/{v1} {v2}/{v2}/{v2} {v3}/{v3}/{v3}\v";
+ 
+                    formatPos +=  $"f {v1}/{}/{} {v2}/{}/{} {v3}/{}/{}\n";
                 }
 
                 // // export normals
                 // for (int i = 0; i < mesh_vertices.Length; i++)
-                //    formatPos += string.Format("vn {0} {1} {2}", mesh_vertices[i].x, mesh_vertices[i].y, mesh_vertices[i].z) + "\v";
+                //    formatPos += string.Format("vn {0} {1} {2}", mesh_vertices[i].x, mesh_vertices[i].y, mesh_vertices[i].z) + "\n";
                 // // export UVs
                 // for (int i = 0; i < mesh_vertices.Length; i++)
-                //    formatPos += string.Format("vt {0} {1}", mesh_vertices[i].x, mesh_vertices[i].y) + "\v";
+                //    formatPos += string.Format("vt {0} {1}", mesh_vertices[i].x, mesh_vertices[i].y) + "\n";
 
                 vertexOffset += mesh_vertices.Length;
             }
